f15e8d2f4cbc0f72751ee48805ff76d3c18dc257
ikylios <ikylioss@gmail.com>
2020-07-05 19:44:01 +0300

    2.11 fix

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -31,11 +31,6 @@ const PersonForm = ({ newName, handleNameChange, newNumber, handleNumberChange, 
 const App = () => {
 
   const [ persons, setPersons] = useState([
-/*    { name: 'Arto Hellas' , number: '040-1231244' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    */
   ]) 
 
   useEffect(() => {
