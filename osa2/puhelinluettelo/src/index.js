cb7094bb37fd9282bb8a45548e1da1789580217b
ikylios <ikylioss@gmail.com>
2020-07-05 19:43:19 +0300

    2.11

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FilterForm = ({ handleFilterChange, filter }) => {
   return (
@@ -28,12 +29,25 @@ const PersonForm = ({ newName, handleNameChange, newNumber, handleNumberChange, 
 }
 
 const App = () => {
+
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '040-1231244' },
+/*    { name: 'Arto Hellas' , number: '040-1231244' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    */
   ]) 
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+    })
+  }, [])
+
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
