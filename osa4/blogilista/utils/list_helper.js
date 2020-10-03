997a42c22a748de199ff611da1b2fed1f1ea6fcc
ikylios <ikylioss@gmail.com>
2020-10-03 20:36:08 +0300

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
    return 1 
   }
+
+  const totalLikes = (blogs) => {
+    const r = (sum, item) => { return sum + item.likes } 
+    return blogs.reduce(r, 0) 
+  }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
