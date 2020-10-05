2d855e16ba494b5988b97a8a21daf3e8f13d537d
ikylios <ikylioss@gmail.com>
2020-10-05 04:36:16 +0300

    4.10

--- osa4/blogilista/utils/blogit_api.test.js
+++ osa4/blogilista/utils/blogit_api.test.js
@@ -2,14 +2,64 @@ const listHelper = require('../utils/list_helper')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Blog = require('../models/blog')
 const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = listHelper.initBlogs
+    .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+
 
 test('notes returned as json', async () => {
     const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(4)
+    expect(response.body).toHaveLength(listHelper.initBlogs.length)
 })
+
+test('adding valid blog works', async () => {
+    const newBlog = {
+        title: 'new',
+        author: 'me',
+        url: 'rlylongurl',
+        likes: 3 
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.url)
+
+    expect(response.body).toHaveLength(listHelper.initBlogs.length + 1)
+})
+
+/* 4.9
+test('id of blog is called id', async () => {
+    const response = await api.get('/api/blogs')
+
+    //console.log(response)
+/*
+    response.set('toJSON', {
+        transform: (document, returnedObject) => {
+          returnedObject.id = returnedObject._id.toString()
+          delete returnedObject._id
+          delete returnedObject.__v
+        }
+      })
+
+    expect(response).toBeDefined()
+})
+*/
 
 afterAll(() => {
     mongoose.connection.close()
