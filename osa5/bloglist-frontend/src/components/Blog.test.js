f33951f4b934fe4d163edba2d5f742318d2e6a53
ikylios <ikylioss@gmail.com>
2020-10-27 19:36:11 +0200

    5.16

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { fireEvent, render } from '@testing-library/react'
 //import { prettyDom } from '@testing-library/dom'
 import Blog from './Blog'
+import BlogForm from './BlogForm'
 
 const userId = {
   username: 'username'
@@ -67,5 +68,35 @@ test('like button is pushed exactly twice', () => {
 
   expect(mockHandler.mock.calls.length).toBe(2)
 
+})
+
+test('blogform calls return when blog created', () => {
+  const createBlog = jest.fn()
+
+  const component = render(
+    <BlogForm handleNewBlog={createBlog} />
+  )
+
+  const author = component.container.querySelector('#author')
+  const title = component.container.querySelector('#title')
+  const url = component.container.querySelector('#url')
+
+  const form = component.container.querySelector('form')
+
+  fireEvent.change(author, {
+    target: { value: 'mikke mekkilson' }
+  })
+  fireEvent.change(title, {
+    target: { value: 'watch out for errors' }
+  })
+  fireEvent.change(url, {
+    target: { value: 'errors.com' }
+  })
+  fireEvent.submit(form)
+
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0].author).toBe('mikke mekkilson' )
+  expect(createBlog.mock.calls[0][0].title).toBe('watch out for errors')
+  expect(createBlog.mock.calls[0][0].url).toBe('errors.com')
 })
 
