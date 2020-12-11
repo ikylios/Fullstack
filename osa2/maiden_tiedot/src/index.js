0297667ca56c583e77d5f94154676c4f337ce88e
ikylios <ikylioss@gmail.com>
2020-12-11 21:58:53 +0200

    2.12

--- osa2/maiden_tiedot/src/index.js
+++ osa2/maiden_tiedot/src/index.js
@@ -4,77 +4,73 @@ import './index.css';
 import axios from 'axios'
 
 const Country = ({ country }) => {
+//  console.log('arrived in Country', country)
   return(
     <div>
       <h2>{country.name}</h2>
-      Capital {country.capital}
+      Capital {country.capital}<br></br> 
       Population {country.population}
       <h3>Languages</h3>
-      {country.languages.map(language => language.name)}
+      <ul>
+      {country.languages.map(language => <li key={country.id}> {language.name} </li>)}
+      </ul>
+      <img src={country.flag} height='100'/>  
+
     </div>
   )
 }
 
-const App = () => {
-  
-  const [ filter, setFilter ] = useState('')
-  const [ countries, setCountries ] = useState([])
-  
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
+const Content = ({ content }) => {
+
+  console.log('content', content)
+
+  const showCountry = (content) => {
+    return <Country country={content} />
   }
   
+  if (content.length > 10) {
+    return 'Too many matches'
+  }
+
+  if (content.length === 1) {
+    return showCountry(content[0])
+  }
+
+  if (content.length <= 10) {
+    return content
+      .map(country => <p key={country.id}> {country.name} <button>Show</button> </p>)
+  }
+} 
+
+const App = () => {
+  
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => { 
-        console.log('servulta saatu', response.data)
         setCountries(response.data)
       })
   }, [])
-
-
-  const [content, setContent] = useState('Too many matches')
-//  console.log('countries', countries)
-  const list = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
-          .map(country => <p key={country.id}> {country.name} 
-          <button onClick={() => showCountry(country.id)}>Show</button> </p>
-          )
-  console.log('list', list)
-//  setContent(countries) 
-//  setContent
-  console.log('content', content)
-
-/*  
-  if (content.length === 1) {
-    setContent(<Country country={content[0]} />)
-  }
-
-  if (list.length <= 10) {
-      console.log('length', content)
-
-      setContent(list)
-/*
-      setContent(
-        list
-          .filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
-          .map(country => <p key={country.id}> {country.name} 
-          <button onClick={() => showCountry(country.id)}>Show</button> </p>
-          )
-      )
-  }
-  */
-
-  const showCountry = (id) => {
-    setContent(<Country country={content[id]} />)
-  }
   
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const list = countries
+    .filter(country => country.name.toLowerCase()
+    .includes(filter.toLowerCase()))
+
+  console.log('list', list)
+
   return (
     <div>
       <form>
         Find countries <input value={filter} onChange={handleFilterChange}/>
       </form>  
-      {list}
+      <Content content={list} />
     </div>
   )
 }
