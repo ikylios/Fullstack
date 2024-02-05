5f45cecc5e3a16d70f05bcac8f68a31818e713b0
xilxilx <ikylioss@gmail.com>
2024-02-05 16:38:40 +0200

    Some fixes and 8.18, 8.19

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,16 +1,32 @@
-import React from "react"
+import { useState } from "react"
 
 const Books = (props) => {
+  const [selectedGenre, setSelectedGenre] = useState("all genres")
+
   if (!props.show) {
     return null
   }
 
   const books = props.books ?? []
 
+  const genresByBook = books.map((book) => book.genres).flat()
+  const genres = genresByBook
+    .filter((genre, index) => genresByBook.indexOf(genre) === index)
+    .concat("all genres")
+    .sort()
+
+  const filteredBooks =
+    selectedGenre == "all genres"
+      ? books
+      : books.filter((book) => book.genres.includes(selectedGenre))
+
   return (
     <div>
       <h2>books</h2>
 
+      {genres.map((genre) => (
+        <button onClick={() => setSelectedGenre(genre)}>{genre}</button>
+      ))}
       <table>
         <tbody>
           <tr>
@@ -18,7 +34,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((b) => (
+          {filteredBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
