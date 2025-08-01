7c14659f5bd24bc0b1dbbb12b842d47d9a9c6434
xilxilx <ikylioss@gmail.com>
2025-08-01 19:10:54 +0300

    13.11

--- osa13/postgres_app/controllers/blogs.js
+++ osa13/postgres_app/controllers/blogs.js
@@ -1,8 +1,27 @@
 const router = require("express").Router()
 const { Blog } = require("../models")
+const { User } = require("../models")
+
+const jwt = require("jsonwebtoken")
+const { SECRET } = require("../util/config")
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization")
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      console.log(authorization.substring(7))
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error) {
+      console.log(error)
+      return res.status(401).json({ error: "token invalid" })
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" })
+  }
+  next()
+}
 
 router.get("/", async (req, res) => {
-  console.log("dsfjkldsjfklds")
   try {
     const blogs = await Blog.findAll()
     console.log(JSON.stringify(blogs, null, 2))
@@ -12,10 +31,10 @@ router.get("/", async (req, res) => {
   }
 })
 
-router.post("/", async (req, res, next) => {
+router.post("/", tokenExtractor, async (req, res, next) => {
   try {
-    console.log(req.body)
-    const blog = await Blog.create(req.body)
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({ ...req.body, userId: user.id })
     console.log(JSON.stringify(blog, null, 2))
     return res.json(blog)
   } catch (error) {
@@ -23,14 +42,20 @@ router.post("/", async (req, res, next) => {
   }
 })
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", tokenExtractor, async (req, res, next) => {
   try {
-    console.log(req.params)
-    await Blog.destroy({ where: { id: req.params.id } })
+    const blog = await Blog.findOne({ where: { id: req.params.id } })
+    console.log(blog.userId)
+    console.log(req.decodedToken.id)
+    if (blog.userId !== req.decodedToken.id) {
+      return res
+        .status(403)
+        .json({ error: "Only allowed to delete own blogs." })
+    }
+    await blog.destroy({ where: { id: req.params.id } })
     return res.status(204).end()
   } catch (error) {
-    console.log(error)
-    return res.status(400).json({ error })
+    next(error)
   }
 })
 
