c02979c327c1bd87933360678f6a3a0914e9a065
xilxilx <ikylioss@gmail.com>
2025-08-01 17:45:54 +0300

    13.10

--- osa13/postgres_app/util/config.js
+++ osa13/postgres_app/util/config.js
@@ -2,8 +2,10 @@ require("dotenv").config()
 
 const PORT = process.env.PORT || 3001
 const DATABASE_URL = process.env.DATABASE_URL
+const SECRET = "pöö"
 
 module.exports = {
   PORT,
   DATABASE_URL,
+  SECRET,
 }
