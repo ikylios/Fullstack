722c73b20e784f419604d82d834af734b9c19ce1
xilxilx <ikylioss@gmail.com>
2025-08-03 14:39:08 +0300

    13.16

--- osa13/postgres_app/index.js
+++ osa13/postgres_app/index.js
@@ -8,13 +8,35 @@ const { errorHandler } = require("./util/errorhandler")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
 const loginRouter = require("./controllers/login")
+const { Blog } = require("./models")
 
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/login", loginRouter)
-app.use(errorHandler)
 
+app.get("/api/authors", async (req, res, next) => {
+  if (req.query.search) {
+    fields = {}
+  }
+
+  try {
+    const agg = await Blog.findAll({
+      group: "author",
+      attributes: [
+        "author",
+        [Blog.sequelize.fn("COUNT", Blog.sequelize.col("id")), "blogs"],
+        [Blog.sequelize.fn("SUM", Blog.sequelize.col("likes")), "likes"],
+      ],
+    })
+    console.log(JSON.stringify(agg, null, 2))
+    res.json(agg)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use(errorHandler)
 const startServer = async () => {
   await connectToDatabase()
   app.listen(PORT, () => {
